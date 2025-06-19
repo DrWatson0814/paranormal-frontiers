402bb14009042ba66b4aab0c1cd805f7f510e096
unknown <shirleyhaddix82@outlook.com>
2025-06-19 16:02:12 -0400

    Began coding Kaggle API

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.static("/api/haunted_places"));
+// app.use(express.static("./api/haunted_places"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-// app.get('/api/haunted_places.csv', (request, response) => {
-//     response.json(locationData);
-// });
+app.get('/api/haunted_places.csv', (request, response) => {
+    response.json(locationData);
+});
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
