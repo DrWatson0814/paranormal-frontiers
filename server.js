2893120121407b392b8de55098c2ea3148ece5dc
unknown <shirleyhaddix82@outlook.com>
2025-06-18 23:13:40 -0400

    Working on hauntedLocation API

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+'use strict'
+
 const express = require('express');
 const app = express();
-const port = 8500;
+const port = 8080;
 
+app.use(express.static("/api/haunted_places"));
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+// app.get('/api/haunted_places.csv', (request, response) => {
+//     response.json(locationData);
+// });
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
