966c1a3ca809891579bec7382ba4f5c53b19e2aa
unknown <shirleyhaddix82@outlook.com>
2025-07-13 16:51:56 -0400

    Scheduled Commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,84 +1,94 @@
 
 
- 
+ //* YouTube Fetch
 
 
-    // var tag = document.createElement('script');
-    // tag.src = "https://www.youtube.com/iframe_api";
-    // var firstScriptTag = document.getElementsByTagName('script') [0];
-    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script') [0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// async function youTubeFetch() {
-//     const response = await fetch('https://www.youtube.com/iframe_api');
-//     const data = await response.json();
-//     console.log(data);
+async function youTubeFetch() {
+    const response = await fetch('https://www.youtube.com/iframe_api');
+    const data = await response.json();
+    console.log(data);
 
-// }
+}
 
-// youTubeFetch();npm
+youTubeFetch();npm
 
-// var player;
-
-
-// function onYoutubeIframeAPIReady() {
-//     player = new YT.Player('player', {
-//         height: '390',
-//         width: '640',
-//         videoId: 'v6RWSfGiDuQ',
-//         playerVars: {
-//             'playsinline': 1,
-//             'autoplay': 0,
-//             'controls': 1,
-//             'showInfo': 0,
-//             'rel': 0,
-//         },
-//         events: {
-//             'onReady': onPlayerReady,
-//             'onStateChange': onPlayerStateChange
-//         }
-//     });
-// }
+var player;
 
 
-// function onPlayerReady(event) {
-//     event.target.playVideo();
-// }
+function onYoutubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'v6RWSfGiDuQ',
+        playerVars: {
+            'playsinline': 1,
+            'autoplay': 0,
+            'controls': 1,
+            'showInfo': 0,
+            'rel': 0,
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
 
 
-// let done = false;
-// function onPlayerStateChange() {
-//     if(event.data == YT.PlayerState.PLAYING && !done) {
-//         setTimeout(stopVideo, 6000);
-//         done = true;    
-//     }
-// }
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
 
-// function stopVideo() {
-//     player.stopVideo();
-// }
+
+let done = false;
+function onPlayerStateChange() {
+    if(event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;    
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
 
 
 
 //* New Video on Refresh //
 
-"use strict";
+"use strict"
 
-const elements = {
-  hauntedPlace: document.getElementsByClassName("haunted"),
-};
+const csvURL = 'http://localhost:8080/public/haunted.places.csv';
 
-const nuggs = [];
+Papa.Parser(csvURL, {
+    download:true,
+    header:true,
+    dynamicTyping: true,
+    complete: function(results) {
+        const hauntedPLaces = results.data;
+        const hauntedList = document.getElementsByClassName('haunted'); 
+        console.log(hauntedList);
+        
+    }
+});
 
 
-function loopHauntedPlaces() {
-    let seqNum = 0;
-    setInterval(() => {
-        if(seqNUm < nuggs.length) {
-            elements.hauntedPlace.textContent = nuggs[0].hauntedPlace
-            nuggs++;
-        } else {
-            nuggs = 0;
-        }
-    }, 3000);
+const elements = document.getElementsByClassName('haunted'),
+
+async function getHauntedPlace() {
+    const endpoint = '/public/haunted_places.csv';
+    const hauntedPlaces = [];
+
+    const response = await fetch(endpoint);
+    const hauntedPlace = response.json();
+    const currentPlace = hauntedPlace.textCOntent(currentPlace);
+    const randomPlace = Math.floor(Math.random() * hauntedPlaces.length);
+    return randomPlace;
 }
-loopHauntedPlaces()
+    
+    
--- server.js
+++ server.js
@@ -1,27 +1,53 @@
 'use strict'
 
 import express, { json, urlencoded } from 'express';
-import path from 'path';
-const bodyParser = ('body-parser');
-const fs = ('fs');
-const csvParser = ('csv-parser');
+import path, { resolve } from 'path';
+import fs from 'fs';
+import csvParser from 'csv-parser';
 
 
 const app = express();
-const port = 8080;
-// const hauntedList = url('/api/haunted_places.csv');
+const PORT = 8080;
 
-app.use(express.static("/api/haunted_places.csv"));
+app.use(express.static("public"));
 app.use(json());
 app.use(urlencoded({extended: false}));
-app.get("./public/js/index.js", (request, response) => {
-    response.json();
-app.get('/api/haunted_places.csv', (request, response) => {
-    const filePath = path.resolve(__dirname, 'haunted_places.csv');
-    response.sendFile('/api/haunted_places.csv');
+// app.get("./public/js/index.js", (request, response) => {
+//     response.json();
+
+
+async function getHauntedPlace() {
+    const endpoint = './public/haunted_places.csv';
+    const hauntedPlaces = [];
+
+    return new Promise ((resolve, reject) => {
+        fs.createReadStream(endpoint)
+        .pipe(csvParser())
+        .on('data',(data) => hauntedPlaces.push(data))
+        .on('end', () => {
+                 const randomPlace = Math.floor(Math.random() * hauntedPlaces.length);
+                 resolve(hauntedPlaces[randomPlace]);
+        });
+    });
+};
+        
+
+app.get('/api/v1/getHauntedPlace', async (request, response) => {
+    try {
+        const hauntedPlace = await getHauntedPlace();
+        response.status(200).json({
+            status: 200,
+            data: hauntedPlace,
+    });
+    } catch (error) {
+        console.error("Something went wrong!", error);
+        response.status(500).json({
+            status: 500,
+            message: "Server Slipped"
+        });
+    }
 });
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-});
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
 });
