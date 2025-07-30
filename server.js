0d4f02c23d5c22e1a67cf1e6ed4c3a506fd50b10
unknown <shirleyhaddix82@outlook.com>
2025-07-30 16:17:40 -0400

    Wiki API Written. Debugging NOw

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/api/v1/fetchVideos', async (request, response) => {
 
 
 async function wikiSearch(searchQuery) {
-  const endpoint = "https://en.wikipedia.org/w/api.php";
+  var endpoint = "https://en.wikipedia.org/w/api.php";
   var params = new URLSearchParams({
     action: "query",
     list: "search",
@@ -106,6 +106,37 @@ async function wikiSearch(searchQuery) {
     console.error("Paranormal Interference - Search Failed", error);
   }
 }
+
+
+async function wikiText(wikiTitle) {
+    const endpoint = "https://en.wikipedia.org/w/api.php";
+    const params = new URLSearchParams({
+        action: 'query',
+        prop: 'extracts',
+        titles: wikiTitle,
+        exintro: true,
+        explaintext: true,
+        format: 'json',
+        origin: '*',
+        redirects: -1
+    });
+
+    try {
+        const response = await fetch(`${endpoint}?${params.toString}`);
+        const dataText = await response.json();
+        const pageID = Object.keys(dataText.query.pages)[0];
+        const extracted = dataText.query.pages[pageID].extracted;
+        console.log(extracted);
+        return extracted;
+    }
+    catch(error) {
+        console.error("Paranormal Interference - Text Extraction Failed", error);
+    }
+
+}
+
+wikiText();
+
 
 
 
